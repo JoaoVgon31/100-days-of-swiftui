6e67199bceee58a96b167d6b630b5c4cf51f7103
João Vitor Gonçalves Oliveira <joaovitorgoliveira31@gmail.com>
2023-05-10 11:39:47 -0300

    Created first project

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by João Vitor Gonçalves Oliveira on 10/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
